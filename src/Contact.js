38f98ed2b70fc70e611057d924775f998227b08b
Isak <fagerlund.isak@gmail.com>
2018-01-26 19:46:37 +0100

    contact page big screen

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { animNavbar } from "./ContactAnimations";
+import isse from "./img/isse.svg";
+import hoggi from "./img/hoggi.svg";
 
 class Contact extends Component {
   componentDidMount() {
@@ -9,6 +11,27 @@ class Contact extends Component {
   render() {
     return (
       <div className="wrapper-contact">
+        <div className="contact-content">
+          <div className="contact-content-isse">
+            <img className="img-fluid" src={isse} alt="worker-one"/>
+            <p>Isak Fagerlund</p>
+            <span>+46 070893472</span>
+          </div>
+          <div className="contact-content-text">
+            <p>We would love to hear from you! Are you interested in free coffee, exploring how we can support your business, our previous work or do you have a project proposal?</p>
+            <p>Just submit your name and email and we’ll get in touch!</p>
+            <a className="contact-button" href="mailto:contact@iskall.io">Message us!</a>  
+          </div>
+          <div className="contact-content-hoggi">
+            <img className="img-fluid" src={hoggi} alt="worker-two"/>
+            <p>Isak Höglund</p>
+            <span>+46 070893472</span>
+          </div>
+        </div>
+        <div className="contact-footer">
+          <h3>Iskall AB</h3>
+          <p>© 2018 Iskall AB | hello@iskall.io | 036-123 123</p>
+        </div>
       </div>
     );
   }
