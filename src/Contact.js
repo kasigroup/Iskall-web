7d54c43da381bce6c604b37696c96f2db878a058
Isak <fagerlund.isak@gmail.com>
2018-01-26 20:44:12 +0100

    process big screen

--- src/Contact.js
+++ src/Contact.js
@@ -18,7 +18,8 @@ class Contact extends Component {
             <span>+46 070893472</span>
           </div>
           <div className="contact-content-text">
-            <p>We would love to hear from you! Are you interested in free coffee, exploring how we can support your business, our previous work or do you have a project proposal?</p>
+            <p>We would love to hear from you!</p>
+            <p>Are you interested in free coffee, exploring how we can support your business, our previous work or do you have a project proposal?</p>
             <p>Just submit your name and email and we’ll get in touch!</p>
             <a className="contact-button" href="mailto:contact@iskall.io">Message us!</a>  
           </div>
--- src/Process.js
+++ src/Process.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 import { animNavbar } from "./ProcessAnimations";
+import table from "./img/table.svg";
+import plane from "./img/plane.svg";
+import define from "./img/define.svg";
+import design from "./img/design.svg";
+import develop from "./img/develop.svg";
+
 
 class Process extends Component {
   componentDidMount() {
@@ -10,8 +16,37 @@ class Process extends Component {
     return (
       <div className="wrapper-process">
         <div className="process-left">
+          <div className="process-left-content">
+            <div className="process-box">
+              <img src={define} alt="define"/>
+              <div className="process-box-text">
+                <h3>Define</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+              </div>
+            </div>
+            <div className="process-box">
+              <img src={design} alt="design"/>
+              <div className="process-box-text">
+                <h3>Design</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+              </div>
+            </div>
+            <div className="process-box">
+              <img src={develop} alt="develop"/>
+              <div className="process-box-text">
+                <h3>Develop</h3>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. </p>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="process-right">
+          <div className="process-plane">
+            <img src={plane} alt="table"/>
+          </div>
+          <div className="process-table">
+            <img className="img-fluid" src={table} alt="table"/>
+          </div>
         </div>
       </div>
     );
